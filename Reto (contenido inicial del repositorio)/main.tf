d7ffdd1025d72d69945519baa619afb947628e11
Edward Suarez <edsuarez0299@gmail.com>
2024-02-09 23:19:51 -0500

    First commit

---
+++ Reto (contenido inicial del repositorio)/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "workshop-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucket_name[0]
+}
+
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = var.bucket_name[1]
+}
